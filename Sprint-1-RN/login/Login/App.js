944728f3ee347e6859a58829039001aa7d9eb0e7
Thiago Domingues Rafael <47195701867@info.local>
2024-01-26 15:58:28 -0300

    tentativa de adicionar fonte

--- Sprint-1-RN/login/Login/App.js
+++ Sprint-1-RN/login/Login/App.js
@@ -1,90 +1,100 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Image, Touchable } from 'react-native';
 import { TouchableOpacity } from 'react-native-web';
+import { useFonts, Poppins_400Regular } from '@expo-google-fonts/inter';
 
 export default function App() {
+  export default () => {
+    let [fontsLoaded] = useFonts({
+  
+      Poppins_400Regular,
+  
+    });
+  
   return (
 
-    <View style={styles.container}>
+
+      <View style={styles.container}>
 
 
-      <Image style={styles.image} source={require('./src/assets/img/Slime_Rosa.webp')} />
+        <Image style={styles.image} source={require('./src/assets/img/Slime_Rosa.webp')} />
 
 
-      <Text style={fonte.Text}>Login</Text>
-      <StatusBar style="auto" />
+        <Text style={fonte.Text}>Login</Text>
+        <StatusBar style="auto" />
 
-      <TextInput
-        style={styles.input}
-        placeholder='Email'
-      />
+        <TextInput
+          style={styles.input}
+          placeholder='Email'
+        />
 
-      <TextInput
-        style={styles.input}
-        placeholder='Senha'
-      />
-      <TouchableOpacity>
+        <TextInput
+          style={styles.input}
+          placeholder='Senha'
+        />
+        <TouchableOpacity>
 
-      <text style={styles.esqueceu}>Esqueceu a senha?</text>
-      </TouchableOpacity>
+          <text style={styles.esqueceu}>Esqueceu a senha?</text>
+        </TouchableOpacity>
 
-      <TouchableOpacity style = {styles.logarButton}>
-        <Text style = {styles.logarText}>Logar</Text>
-    </TouchableOpacity>
+        <TouchableOpacity style={styles.logarButton}>
+          <Text style={styles.logarText}>Logar</Text>
+        </TouchableOpacity>
 
 
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#add8e6',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    width: '70%',
-    height: 40,
-    borderWidth: 1,
-    padding: 10,
-    marginTop: 10,
-    marginBottom: 10,
-    borderRadius: 10,
-    backgroundColor: 'white',
-    borderColor: 'white'
-  },
-  image: {
-    width: 100,
-    height: 100,
-    marginBottom: 30,
-  },
-  logarButton: {
-    color: 'white',
-    fontWeight: 500,
-    marginTop: 40,
-    borderWidth: 1,
-    padding: 10,
-    borderRadius: 13,
-    borderColor: '#fff',
-    backgroundColor: '#fff'
-  },
-  logarText: {
-    color: 'black',
-    justifyContent: 'center',
-    alignContent: 'center'
-  },
-  esqueceu: {
-    marginTop: 8,
-    fontSize: 12,
+      </View>
+    );
   }
-});
 
-const fonte = StyleSheet.create({
-  Text: {
-    fontSize: 20,
-    marginBottom: 20,
-    marginTop: 100,
-  }
-})
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#add8e6',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+      width: '70%',
+      height: 40,
+      borderWidth: 1,
+      padding: 10,
+      marginTop: 10,
+      marginBottom: 10,
+      borderRadius: 10,
+      backgroundColor: 'white',
+      borderColor: 'white',
+      fontFamily: 'Poppins_400Regular'
+    },
+    image: {
+      width: 100,
+      height: 100,
+      marginBottom: 30,
+    },
+    logarButton: {
+      color: 'white',
+      fontWeight: 500,
+      marginTop: 40,
+      borderWidth: 1,
+      padding: 10,
+      borderRadius: 13,
+      borderColor: '#fff',
+      backgroundColor: '#fff'
+    },
+    logarText: {
+      color: 'black',
+      justifyContent: 'center',
+      alignContent: 'center'
+    },
+    esqueceu: {
+      marginTop: 8,
+      fontSize: 12,
+    }
+  });
+
+  const fonte = StyleSheet.create({
+    Text: {
+      fontSize: 20,
+      marginBottom: 20,
+      marginTop: 100,
+    }
+  })}
